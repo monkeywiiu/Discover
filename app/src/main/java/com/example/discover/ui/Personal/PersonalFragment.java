e2433d3c8acca978caa3b1c1ebfc7e524db484e5
y294582253 <294582253@qq.com>
2017-12-30 23:35:45 +0800

    添加个人中心fragment

---
+++ app/src/main/java/com/example/discover/ui/Personal/PersonalFragment.java
@@ -0,0 +1,8 @@
+package com.example.discover.ui.Personal;
+
+/**
+ * Created by monkeyWiiu on 2017/12/30.
+ */
+
+public class PersonalFragment {
+}
---
+++ app/src/main/java/com/example/discover/utils/ShareUtil.java
@@ -0,0 +1,8 @@
+package com.example.discover.utils;
+
+/**
+ * Created by monkeyWiiu on 2017/12/29.
+ */
+
+public class ShareUtil {
+}
